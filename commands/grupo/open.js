36061a07a19549589685d372fbe589f9cbcbd192
Kayn <necrohek98@gmail.com>
2026-04-11 18:56:39 -0600

    Create open.js

---
+++ commands/grupo/open.js
@@ -0,0 +1,24 @@
+export default {
+  command: ['open', 'abrir'],
+  category: 'grupo',
+  isAdmin: true,
+  botAdmin: true,
+  run: async (client, m) => {
+    try {
+      const groupMetadata = await client.groupMetadata(m.chat)
+      const groupAnnouncement = groupMetadata.announce
+
+      if (groupAnnouncement === false) {
+        m.react('✖️')
+        //return client.reply(m.chat, `🇪🇪 El grupo ya está abierto.`, m)
+      }
+
+      await client.groupSettingUpdate(m.chat, 'not_announcement')
+      m.react('✔️')
+      //return client.reply(m.chat, `🇪🇪 El grupo ha sido abierto correctamente.`, m)
+    } catch (err) {
+      console.error(err)
+      return client.reply(m.chat, msgglobal, m)
+    }
+  },
+};

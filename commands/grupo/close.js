f70b12b7eac8bb3fc327f1f1a80c87b1beb458b0
Kayn <necrohek98@gmail.com>
2026-04-11 18:53:42 -0600

    Create close.js

---
+++ commands/grupo/close.js
@@ -0,0 +1,24 @@
+export default {
+  command: ['close', 'cerrar'],
+  category: 'grupo',
+  isAdmin: true,
+  botAdmin: true,
+  run: async (client, m) => {
+    try {
+      const groupMetadata = await client.groupMetadata(m.chat)
+      const groupAnnouncement = groupMetadata.announce
+
+      if (groupAnnouncement === true) {
+        m.react('🇪🇪')
+        //return client.reply(m.chat, `✖️ *El grupo ya está cerrado.*`, m)
+      }
+
+     await client.groupSettingUpdate(m.chat, 'announcement')
+      m.react('🇪🇪')
+      //return client.reply(m.chat, `✔️ *El grupo ha sido cerrado correctamente.*`, m)
+    } catch (err) {
+      console.error(err)
+      return client.reply(m.chat, msgglobal, m)
+    }
+  },
+};
